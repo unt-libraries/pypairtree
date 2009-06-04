e848c474a476ea25f0b1d482b4916879de65aa5c
Lauren Ko <lauren.ko@unt.edu>
2009-06-04 16:28:30 +0000

    pairtreelist.py: initial import

---
+++ pairtreelist.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import sys
+import os
+import pairtree
+from optparse import OptionParser
+
+
+def listIDs(basedir):
+    """Lists digital object identifiers of Pairtree directory structure.
+
+    Walks a Pairtree directory structure to get IDs. Prepends prefix found in 
+    pairtree_prefix file. Outputs to standard output.
+    """
+    prefix = ""
+    # check for pairtree_prefix file
+    prefixfile = os.path.join(basedir, 'pairtree_prefix')
+    if os.path.isfile(prefixfile):
+        rff = open(prefixfile, 'r')
+        prefix = rff.readline().strip()
+        rff.close() 
+    # check for pairtree_root dir
+    root = os.path.join(basedir, 'pairtree_root')
+    if os.path.isdir(root):
+        objects = pairtree.findObjects(root)
+        for obj in objects:
+            doi = os.path.split(obj)[1]
+            # print with prefix and original chars in place
+            print prefix + pairtree.deSanitizeString(doi)
+    else:
+        print 'pairtree_root directory not found'
+
+
+if __name__ == "__main__":
+    usage = "usage: %prog [options] <pairtreedir>"
+    parser = OptionParser(usage)
+    (options, args) = parser.parse_args()
+    if len(args) != 1:
+        parser.print_help()
+        sys.exit(1)
+    listIDs(args[0])
